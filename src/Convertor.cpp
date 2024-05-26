edbd61e9e2f5972bd65f18968c00cd26fa4e05cf
leinlix <748741358@qq.com>
2024-05-26 21:31:14 +0800

    feature: add some shape inference

--- src/Convertor.cpp
+++ src/Convertor.cpp
@@ -85,7 +85,7 @@ namespace LLX{
                         std::cout<< "error in tensor output node:" << op->name <<std::endl;
                     }
                     std::string node_output_name = mnn_net_ptr->tensorName[output_idx];
-                    node->add_input(node_output_name);
+                    node->add_output(node_output_name);
                     // find extraTensorDescribe First
                     MNN::TensorDescribeT * tensor_describe = nullptr;
                     if(mnn_net_ptr->extraTensorDescribe.size() > output_idx){
@@ -93,7 +93,149 @@ namespace LLX{
                     }
                     if(tensor_describe == nullptr){
                         // do the shape inference 
-                        // wait to add 
+                        onnx::ValueInfoProto *p = scope->findValueInfoByName(mnn_net_ptr->tensorName[input_idx_for_inference]);
+                        if(p!=nullptr){
+                            onnx::ValueInfoProto *node_output;
+                            if(op->type == MNN::OpType_Convolution || op->type == MNN::OpType_ConvolutionDepthwise){
+                                MNN::Convolution2DT * op_param = (MNN::Convolution2DT *)op->main.value;
+                                 std::vector<int> di;
+                                for(int j = 0; j < 4; j++){
+                                    int dim = p->type().tensor_type().shape().dim()[j].dim_value();
+                                    if(j == 1){
+                                        dim = op_param->common->outputCount;
+                                    }else if(j==2){
+                                        dim = (dim + 2* op_param->common->padX + op_param->common->strideX - ((op_param->common->kernelX -1)*op_param->common->dilateX + 1))/op_param->common->strideX;
+                                    }else if (j == 3){
+                                        dim = (dim + 2* op_param->common->padY + op_param->common->strideY - ((op_param->common->kernelY -1)*op_param->common->dilateY + 1))/op_param->common->strideY;
+                                    }
+                                    di.push_back(dim);
+                                }
+                                node_output = OPConvertor::valueInfoProtoFromArgs(node_output_name,p->type().tensor_type().elem_type(),di.size(),di);
+                            }else if (op->type == MNN::OpType_Pooling){
+                                MNN::PoolT * op_param = (MNN::PoolT *)op->main.value;
+                                std::vector<int> di;
+                                for(int j = 0; j < 4; j++){
+                                    int dim = p->type().tensor_type().shape().dim()[j].dim_value();
+                                    if(j==2){
+                                        dim = (dim + op_param->strideX - op_param->kernelX)/op_param->strideX;
+                                    }else if (j == 3){
+                                        dim = (dim + op_param->strideY - op_param->kernelY)/op_param->strideY;
+                                    }
+                                    di.push_back(dim);
+                                }
+                                node_output = OPConvertor::valueInfoProtoFromArgs(node_output_name,p->type().tensor_type().elem_type(),di.size(),di);
+                            }else if (op->type == MNN::OpType_Pooling3D){
+                                std::vector<int> di;
+                                for(int j = 0; j < 4; j++){
+                                    int dim = p->type().tensor_type().shape().dim()[j].dim_value();
+                                    if(j>1){
+                                        dim = 1;
+                                    }
+                                    di.push_back(dim);
+                                }
+                                node_output = OPConvertor::valueInfoProtoFromArgs(node_output_name,p->type().tensor_type().elem_type(),di.size(),di);
+                            }else if(op->type == MNN::OpType_Interp){
+                                auto input_2_init = scope->findInitializerByName(mnn_net_ptr->tensorName[op->inputIndexes[1]]);
+                                if (input_2_init != nullptr){
+                                    input_2_init->set_data_type(onnx::TensorProto_DataType_INT64);
+                                    std::vector<int> di;
+                                    // change data to int64 and set value
+                                    for(int j = 0 ; j < input_2_init->dims_size(); j++){
+                                        for(int k = 0 ; k < input_2_init->dims(j);k++){
+                                            input_2_init->add_int64_data(input_2_init->int32_data(k));
+                                            di.push_back(input_2_init->int32_data(k));
+                                        }
+                                    }
+                                    input_2_init->clear_int32_data();
+                                    node_output = OPConvertor::valueInfoProtoFromArgs(node_output_name,p->type().tensor_type().elem_type(),di.size(),di);
+                                }else{
+                                    std::cout<<"error in shape inferece for interp, can't find input value"<<std::endl;
+                                }
+                            }else if (op->type == MNN::OpType_Reshape){
+                                auto input_2_init = scope->findInitializerByName(mnn_net_ptr->tensorName[op->inputIndexes[1]]);
+                                if (input_2_init != nullptr)
+                                {
+                                    input_2_init->set_data_type(onnx::TensorProto_DataType_INT64);
+                                    int64_t all = 1;
+                                    for(int j = 0 ; j < p->type().tensor_type().shape().dim_size(); j++){
+                                        all *= p->type().tensor_type().shape().dim()[j].dim_value();
+                                    }
+                                    // change data to int64
+                                    for(int j = 0 ; j < input_2_init->dims_size(); j++){
+                                        for(int k = 0 ; k < input_2_init->dims(j);k++){
+                                            input_2_init->add_int64_data(input_2_init->int32_data(k));
+                                            if(input_2_init->int32_data(k)!=-1){
+                                                all /= input_2_init->int32_data(k);
+                                            }
+                                        }
+                                    }
+                                    input_2_init->clear_int32_data();
+                                    std::vector<int> di;
+                                    for(int j = 0 ; j< input_2_init->int64_data_size(); j++){
+                                        if(input_2_init->int64_data(j) == -1){
+                                            di.push_back(all);
+                                        }else{
+                                            di.push_back(input_2_init->int64_data(j));
+                                        }
+                                    }
+                                    node_output = OPConvertor::valueInfoProtoFromArgs(node_output_name,p->type().tensor_type().elem_type(),di.size(),di);
+                                }else{
+                                    std::cout<<"error in shape inferece for reshap, can't find input value"<<std::endl;
+                                }
+                            }else if(op->type == MNN::OpType_Concat){
+                                MNN::AxisT *axis = (MNN::AxisT *)op->main.value;
+                                auto input_2 = scope->findValueInfoByName(mnn_net_ptr->tensorName[op->inputIndexes[1]]); 
+                                std::vector<int>di ;
+                                for(int j = 0 ; j < input_2->type().tensor_type().shape().dim_size(); j++){
+                                    if(j == axis->axis){
+                                        di.push_back(input_2->type().tensor_type().shape().dim()[j].dim_value() + p->type().tensor_type().shape().dim()[j].dim_value() )
+                                    }else{
+                                        di.push_back(input_2->type().tensor_type().shape().dim()[j].dim_value());
+                                    }
+                                }
+                                node_output = OPConvertor::valueInfoProtoFromArgs(node_output_name,p->type().tensor_type().elem_type(),di.size(),di);
+                            }else if(op->type == MNN::OpType_ArgMax){
+                                std::vector<int> di;
+                                for(int j = 0 ; j < p->type().tensor_type().shape().dim_size(); j++){
+                                    di.push_back(p->type().tensor_type().shape().dim()[j].dim_value());
+                                }
+                                node_output = OPConvertor::valueInfoProtoFromArgs(node_output_name, onnx::TensorProto_DataType_INT64, di.size(),di);
+                            }else if(op->type == MNN::OpType_Rank){ 
+                                std::vector<int> di ;
+                                node_output = OPConvertor::valueInfoProtoFromArgs(node_output_name, onnx::TensorProto_DataType_INT64, di.size(),di);
+                            }else if (op->type == MNN::OpType_Shape){
+                                std::vector<int> di ;
+                                di.push_back(p->type().tensor_type().shape().dim_size());
+                                node_output = OPConvertor::valueInfoProtoFromArgs(node_output_name, onnx::TensorProto_DataType_INT64, di.size(),di);
+                            }else if(op->type == MNN::OpType_Flatten){
+                                MNN::FlattenT * op_param = (MNN::FlattenT*)op->main.value;
+                                int dim1 = 1, dim2 = 1;
+                                std::vector<int> di;
+                                for(int j = 0; j < p->type().tensor_type().shape().dim_size();j++){
+                                    if(j < op_param->axis){
+                                        dim1 *= p->type().tensor_type().shape().dim(j).dim_value();
+                                    }else {
+                                        dim2 *= p->type().tensor_type().shape().dim(j).dim_value();
+                                    }
+                                }
+                                di.push_back(dim1);
+                                di.push_back(dim2);
+                                node_output = OPConvertor::valueInfoProtoFromArgs(node_output_name,onnx::TensorProto_DataType_INT64,di.size(),di);
+                            }else{
+                                std::vector<int> di;
+                                for(int j = 0 ; j < p->type().tensor_type().shape().dim_size(); j++){
+                                    di.push_back(p->type().tensor_type().shape().dim()[j].dim_value());
+                                }
+                                node_output = OPConvertor::valueInfoProtoFromArgs(node_output_name,p->type().tensor_type().elem_type(),di.size(),di);
+                            }
+                            scope->pushValue(node_output);
+                            // check if it is the model output
+                            if(std::find(mnn_net_ptr->outputName.begin(), mnn_net_ptr->outputName.end(), node_output_name)!=mnn_net_ptr->outputName.end()){
+                                scope->pushOutput(node_output);
+                            }
+                        }else{
+                            std::cout<<"unable inference the output node with no input!"<<std::endl;
+                        }
                     }else{
                         std::vector<int> node_output_dims ;
                         for(int j = 0 ; j < tensor_describe->blob->dims.size(); j++){
